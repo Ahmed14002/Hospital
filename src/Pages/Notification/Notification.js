d123b8d6e5b9dacdca1880df2a55b6d4ebd60304
Ahmed Hossam <125371260+Ahmed14002@users.noreply.github.com>
2024-06-19 13:39:03 +0300

    Update Notification.js

--- src/Pages/Notification/Notification.js
+++ src/Pages/Notification/Notification.js
@@ -2,8 +2,5 @@ import React from 'react';
 import Header from './Header';
 export default function Notification() {
   return (
-    <div className="flex flex-col gap-14">
-      <Header />
-    </div>
-  );
+    <p> Notification </p>
 }
