599615564f27fd5337cbeffd8f4fd40c4c3e29b7
Ahmed Hossam <125371260+Ahmed14002@users.noreply.github.com>
2024-06-19 13:40:15 +0300

    Update Settings.js

--- src/Pages/Settings/Settings.js
+++ src/Pages/Settings/Settings.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import SettingNav from './SettingNav';
 export default function Settings() {
-  return (
-    <div className="flex flex-col gap-20">
-      <SettingNav />
-    </div>
-  );
+  return <p> Settings </p>
 }
