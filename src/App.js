372984486928dc2b5116f832a5223caf632b5e45
Ahmed Hossam <125371260+Ahmed14002@users.noreply.github.com>
2024-06-24 16:34:05 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from './Pages/Auth/Login';
-import Main from './Pages/Main/Main';
-import Home from './Pages/Home/Home';
-import Donors from './Pages/Donors/Donors';
-import Notification from './Pages/Notification/Notification';
-import Settings from './Pages/Settings/Settings';
+// import Login from './Pages/Auth/Login';
+// import Main from './Pages/Main/Main';
+// import Home from './Pages/Home/Home';
+// import Donors from './Pages/Donors/Donors';
+// import Notification from './Pages/Notification/Notification';
+// import Settings from './Pages/Settings/Settings';
+import AppRoutes from './AppRoutes';
 
 
 export default function App() {
   return (
     <div className="App">
-      <Routes>
-       <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Main />}>
-          <Route path="home" element={<Home />} />
-          <Route path="donors" element={<Donors />} />
-          <Route path="notification" element={<Notification />} />
-          <Route path="settings" element={<Settings />} />
-        </Route>
-      </Routes>
+       <AppRoutes />
     </div>
   );
 }
