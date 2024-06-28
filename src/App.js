1fd54ed036a344e8cf0c4c4008980b3cd2eb48d0
Ahmed Hossam <125371260+Ahmed14002@users.noreply.github.com>
2024-06-28 18:25:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-// import Login from './Pages/Auth/Login';
-// import Main from './Pages/Main/Main';
-// import Home from './Pages/Home/Home';
-// import Donors from './Pages/Donors/Donors';
-// import Notification from './Pages/Notification/Notification';
-// import Settings from './Pages/Settings/Settings';
-import AppRoutes from './AppRoutes';
-
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './Pages/Auth/Login';
+import Main from './Pages/Main/Main';
+import Home from './Pages/Home/Home';
+import Donors from './Pages/Donors/Donors';
+import Notification from './Pages/Notification/Notification';
+import Settings from './Pages/Settings/Settings';
+import ProtectedRoute from './Pages/Auth/ProtectedRoutes';
+import RedirectIfAuthenticated from './Pages/Auth/RedirectIfAuthenticated';
+import AccessDenied from './Pages/Auth/AccessDenied';
 
 export default function App() {
   return (
     <div className="App">
-       <AppRoutes />
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <RedirectIfAuthenticated>
+              <Login />
+            </RedirectIfAuthenticated>
+          }
+        />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/donors" element={<Donors />} />
+          <Route path="/notification" element={<Notification />} />
+          <Route element={<ProtectedRoute roles={['MANGER']} />}>
+            <Route path="/settings" element={<Settings />} />
+          </Route>
+        </Route>
+
+        <Route path="/access-denied" element={<AccessDenied />} />
+      </Routes>
     </div>
   );
 }
+
